5d22c7c30e091269fb0a642fcd41a1cd30932ee2
Christo Riekert <christoriekert@gmail.com>
2021-03-21 20:18:24 +0800

    FEAT: added lesson 02

---
+++ csharp-largest-number-02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace csharp_largest_number
+{
+    static class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[3];
+
+            Console.WriteLine("Please enter the first number: ");
+            numbers[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number: ");
+            numbers[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the third number: ");
+            numbers[2] = Convert.ToInt32(Console.ReadLine());
+
+            int largestNumber = 0;
+
+            // using for - https://www.tutorialsteacher.com/csharp/csharp-for-loop
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                int number = numbers[index];
+                if (number > largestNumber)
+                    largestNumber = number;
+            }
+
+            Console.WriteLine($"The largest number using 'for' was: {largestNumber}");
+
+            // using foreach - https://www.tutorialsteacher.com/csharp/array-csharp
+            largestNumber = 0;
+            foreach (int number in numbers)
+            {
+                if (number > largestNumber)
+                    largestNumber = number;
+            }
+
+            Console.WriteLine($"The largest number using 'foreach' was: {largestNumber}");
+
+            // using no loop but sort method - https://www.tutorialsteacher.com/articles/sort-array-in-csharp
+            Array.Sort(numbers);
+
+            // this sorts smallest to biggest so we want the LAST item in the array
+            // remember, arrays are zero (0) based, so 0, 1, 2
+            largestNumber = numbers[2];
+            Console.WriteLine($"The largest number using 'Array.Sort' was: {largestNumber}");
+        }
+    }
+}
---
+++ csharp-largest-number-02/README.md
@@ -0,0 +1,28 @@
+# csharp-largest-number-02
+The 'csharp-largest-number' is the second 'lesson' in a series of programming lessons for my son.  
+
+As new concepts are learned (taught) then a small project accompanies the lesson.  The code here is an
+example of the end result.
+
+## Project/Lesson Requirements
+Write a C# console program that asks the user to enter three numbers, calculate the greatest (biggest)
+of the three and display the greatest one.
+
+### Example 
+If the user entered 34, 5, 27 then the result must be: "The number 34 is the greatest"
+
+### Components/Constructs
+Make use of the following constructs/language features:
+- ```Console.ReadLine()``` - gets input from the user
+- ```Console.WriteLine()``` - writes output to the console
+- ```int[]``` - integer array
+- ```for``` loop to calculate the largest number
+
+The following components are optional:
+- ```foreach``` - loop to calculate the largest number
+- ```Array.Sort()``` - using an array sort to get the largest number
+
+## License
+Copyright (c) MinusTwenty2. All rights reserved.
+
+Licensed under the [MIT License](./LICENSE).
